8a66196a386d8e1afd312727efb8b97b6c16d997
rebeca-august <rebeca.cazacu@gmail.com>
2020-11-16 19:38:14 +0000

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css'
 import Game from './components/Game'
+import './styles.css'
 
 function App() {
-  return <Game />
+  return <Game className="game" />
 }
 
 export default App
--- src/helpers.js
+++ src/helpers.js
@@ -12,12 +12,7 @@ export function calculateWinner(squares) {
 
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i]
-    if (
-      squares[a] &&
-      squares[a] === squares[b] &&
-      squares[a] === squares[c] &&
-      squares[a]
-    ) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a]
     }
   }
